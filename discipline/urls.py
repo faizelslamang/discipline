2289c2a99ccdc5a51bea66d99d250ceb9bfd13b6
Nathan Geffen <nathangeffen@gmail.com>
2017-12-10 17:40:55 +0200

    Front page.

--- discipline/urls.py
+++ discipline/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+#from django.conf.urls import url
+#from django.contrib.auth.decorators import login_required
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('students/', include('students.urls')),
     path('admin/', admin.site.urls),
-    url(r'^$', login_required(TemplateView.as_view(
-        template_name='index.html')), name='home'),
+    path('', RedirectView.as_view(url='admin/'), name='home'),
 ]
